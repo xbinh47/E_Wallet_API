6b806613f6cf7d7723ca75949ee9f867fa28fa62
xbinh47 <binhnguyenxuan47@gmail.com>
2022-12-11 21:12:06 +0700

    Update userdetail.php

--- api/user/userdetail.php
+++ api/user/userdetail.php
@@ -26,28 +26,18 @@
         $sql = "SELECT `idUser`,`email`,`balance`,`phone`,`name`,`birthday`,`address`,`front`,`back`,`createAt`,`updateAt` FROM `users` JOIN `state` ON `users`.`idState` = `state`.`idState`  AND `users`.`phone` = '$phone'";
         $users = executeResult($sql, true);
         if (!empty($users)) {
-            // $sql = "SELECT `phone` FROM `users` WHERE `email` = '$email'";
-            // $phoneSender = executeResult($sql, true);
-            // if($phoneSender['phone'] == $phone){
-            //     die(json_encode(array('code' => 1, 'data' => 'Không thể chuyển tiền cho chính mình')));
-            // }
-            // if ($users['idState'] ==2 ){
-            //     die(json_encode(array('code' => 0, 'data' => $users)));
-            // }else{
-            //     die(json_encode(array('code' => 1, 'data' => 'Người dùng chưa được xác minh tài khoản')));
-            // }
             die(json_encode(array('code' => 0, 'data' => $users)));
         } else {
             die(json_encode(array('code' => 1, 'data' => "Không có người dùng này trong hệ thống")));
         }
     }else{
         $email = $data->email;
-        $sql = "SELECT `idUser`,`email`,`balance`,`phone`,`name`,`birthday`,`address`,`front`,`back`,`createAt`,`updateAt` FROM `users` JOIN `state` ON `users`.`idState` = `state`.`idState`  AND `users`.`phone` = '$phone'";
+        $sql = "SELECT `idUser`,`email`,`balance`,`phone`,`name`,`birthday`,`address`,`front`,`back`,`createAt`,`updateAt` FROM `users` JOIN `state` ON `users`.`idState` = `state`.`idState`  AND `users`.`email`= '$email'";
         $user = executeResult($sql, true);
         if (!empty($user)) {
             die(json_encode(array('code' => 0, 'data' => $user)));
         } else {
-            die(json_encode(array('code' => 1, 'data' => [])));
+            die(json_encode(array('code' => 1, 'data' => "Không có người dùng này trong hệ thống")));
         }
     }
     
